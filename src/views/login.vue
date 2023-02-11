22664b19ba7c169968fd4ec365c2be2bdaddd496
wb <dearbo7@163.com>
2023-02-11 09:52:44 +0800

    去除默认密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -75,9 +75,9 @@ const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 const loginForm = ref({
-  username: "admin",
-  password: "admin123",
-  rememberMe: false,
+  username: "",
+  password: "",
+  rememberMe: true,
   code: "",
   uuid: ""
 });
